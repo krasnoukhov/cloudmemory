99b0342896ed190836eebad9d6e412d765b7d16d
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2013-01-09 01:14:33 +0200

    Configured app and basic layout

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source :rubygems
+
+gem "cuba"
+gem "rack-protection"
+gem "thin"
+gem "clogger"
+gem "tilt"
+gem "slim"
+
+group :development do
+  gem "powify"
+end
+
+group :assets do
+  gem "sprockets"
+  gem "sprockets-sass"
+  gem "sass"
+  gem "compass"
+  gem "coffee-script"
+end
---
+++ app.rb
@@ -0,0 +1,28 @@
+require "cuba"
+require "cuba/render"
+require "slim"
+require "securerandom"
+require "rack/protection"
+require "sprockets"
+require "sprockets-sass"
+require "sass"
+require "compass"
+require "clogger"
+
+Cuba.use Rack::Session::Cookie, secret: SecureRandom.hex(64)
+Cuba.use Rack::Protection
+Cuba.use Clogger, format: :Combined, path: "./log/requests.log", reentrant: true
+Cuba.plugin Cuba::Render
+
+Cuba.define do
+  on root do
+    res.write render("views/index.slim")
+  end
+  
+  on "assets" do
+    sprockets = Sprockets::Environment.new
+    sprockets.append_path "assets/javascripts"
+    sprockets.append_path "assets/stylesheets"
+    run sprockets
+  end
+end
