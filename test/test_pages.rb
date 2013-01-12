be3b8c564c949d63407a32a441429a6424e53bac
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2013-01-12 18:59:59 +0200

    Added photo test

--- test/test_pages.rb
+++ test/test_pages.rb
@@ -1,11 +1,25 @@
 require File.expand_path("helper", File.dirname(__FILE__))
 
 scope do
-  test "Root" do
+  test "Home" do
     visit "/"
     
     assert has_content?("Cloud Memory")
     assert has_content?("Natalia")
     assert has_content?("Dmitry")
+  end
+  
+  test "Photo" do
+    visit "/photo/tags/ruby"
+    json = JSON.parse(body)
+    
+    assert json.include? "caption"
+    assert json.include? "images"
+    
+    visit "/photo/location/48.819263,2.415799"
+    json = JSON.parse(body)
+    
+    assert json.include? "caption"
+    assert json.include? "images"
   end
 end
